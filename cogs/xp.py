0a577b48716ebd10873f8bcdcba18147200d38b3
arslee <me@arslee.dev>
2022-02-01 12:37:59 +0900

    Small code refactor

--- cogs/xp.py
+++ cogs/xp.py
@@ -1,77 +1,75 @@
-import discord
-from discord.ext import commands,tasks
+from discord.ext import commands, tasks
+
 
 class xp(commands.Cog):
-    @tasks.loop(seconds = 120)
+    @tasks.loop(seconds=120)
     async def save_db(self):
         self.client.connection.commit()
-    
-    def __init__(self,client):
+
+    def __init__(self, client):
         self.client = client
         self.client.cursor = client.cursor
         self.client.connection = client.connection
         self.save_db.start()
+
     def cog_unload(self):
         self.save_db.stop()
         self.client.connection.commit()
 
-
-
-
-    async def add_xp(self,userId,guildId,amount):
-
-
+    async def add_xp(self, userId, guildId, amount):
         """adding xp to member (if exists)/adding member to db and setting his xp to X"""
 
-
-        self.client.cursor.execute(f"""SELECT * FROM usersData WHERE userId = {userId} AND guildId = {guildId}""")
+        self.client.cursor.execute(
+            f"SELECT * FROM usersData WHERE userId = {userId} AND guildId = {guildId}"
+        )
         user = self.client.cursor.fetchone()
 
-        if user == None:
-
-            self.client.cursor.execute(f"""
-                INSERT INTO usersData VALUES({userId},{guildId},{amount});
-            """)
-            return 0,amount
+        if user is None:
+            self.client.cursor.execute(
+                f"INSERT INTO usersData VALUES({userId},{guildId},{amount});"
+            )
+            return 0, amount
         else:
 
-            total = int(user[2])+amount
-            self.client.cursor.execute(f"DELETE FROM usersData WHERE userId = {userId} AND guildId = {guildId};")
-            self.client.cursor.execute(f"""
-                
-                INSERT INTO usersData VALUES({userId},{guildId},{total});
-            """)
+            total = int(user[2]) + amount
+            self.client.cursor.execute(
+                f"DELETE FROM usersData WHERE userId = {userId} AND guildId = {guildId};"
+            )
+            self.client.cursor.execute(
+                f"INSERT INTO usersData VALUES({userId},{guildId},{total});"
+            )
 
-            return user[2],total
+            return user[2], total
 
-    async def get_xp(self,userId,guildId):
-
-
+    async def get_xp(self, userId, guildId):
         """getting xp from user"""
 
-
-        self.client.cursor.execute(f"""SELECT * FROM usersData WHERE userId = {userId} AND guildId = {guildId}""")
+        self.client.cursor.execute(
+            f"SELECT * FROM usersData WHERE userId = {userId} AND guildId = {guildId}"
+        )
         user = self.client.cursor.fetchone()
 
-        if user == None:
+        if user is None:
 
-            self.client.cursor.execute(f"""
-                INSERT INTO usersData VALUES({userId},{guildId},0);
-            """)
+            self.client.cursor.execute(
+                f"INSERT INTO usersData VALUES({userId},{guildId},0);"
+            )
             return 0
 
         else:
             return user[2]
 
-
-    """ xp adding (debug) """    
     @commands.Cog.listener()
-    async def on_message(self,message):
-        if message.author.id == self.client.user.id: return False 
-        _,total = await self.add_xp(message.author.id, message.guild.id, len(message.content))
-        if total/5 == round(total/5):
+    async def on_message(self, message):
+        """xp adding (debug)"""
+        if message.author.id == self.client.user.id:
+            return False
+        _, total = await self.add_xp(
+            message.author.id, message.guild.id, len(message.content)
+        )
+        if total / 5 == round(total / 5):
             await message.reply(f"xp amount: {total}")
-    
-    
+
+
 def setup(client):
     client.add_cog(xp(client))
--- loader.py
+++ loader.py
@@ -1,31 +1,29 @@
-from sqlite3 import connect
-import pymysql as mysql,os,time
-from discord.ext import commands 
+import os
+
+import pymysql as mysql
+from discord.ext import commands
 
 connection = mysql.connect(
-    host="127.0.0.1",
-    user="root",
-    password="?",
-    database="xpdata"
+    host="127.0.0.1", user="root", password="?", database="xpdata"
 )
 
 token = "your awesome token :+1:"
 with connection:
     cursor = connection.cursor()
-    
-    cursor.execute("""CREATE TABLE IF NOT EXISTS usersData(
+
+    cursor.execute(
+        """CREATE TABLE IF NOT EXISTS usersData(
         userId VARCHAR(255),
         guildId VARCHAR(255),
         xpValue VARCHAR(255)
-        );""")
-    client = commands.Bot(command_prefix='xp/')
+        );"""
+    )
+    client = commands.Bot(command_prefix="xp/")
     client.cursor = cursor
     client.connection = connection
-    for cog in os.listdir('cogs'):
+    for cog in os.listdir("cogs"):
         if cog.endswith(".py"):
-            client.load_extension(f'cogs.{cog[:-3]}')
+            client.load_extension(f"cogs.{cog[:-3]}")
             print(cog)
-    
-    
+
     client.run(token)
-    
